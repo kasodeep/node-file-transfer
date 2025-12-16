9e7a52ee665cfcee807b50106a9a8ef8558138ed
Kasodariya Deep <deepkasodariya44@gmail.com>
2025-12-16 18:26:05 +0530

    Increase the file upload size

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
+  limits: { fileSize: 500 * 1024 * 1024 }, // 10MB limit
 });
 
 // WebSocket server setup
